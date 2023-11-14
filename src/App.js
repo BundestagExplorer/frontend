c0c31347bbad3d034df5fe0780d49a27442d4d79
ge49kuz <simon.strzoda@tum.de>
2023-11-14 21:31:49 +0100

    working example of d3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import { render } from 'react-dom';
+import WordCloud from 'react-d3-cloud';
+
+const data = [
+  { text: 'Hallo', value: 400 },
+  { text: 'Team!', value: 400 },
+  { text: 'BundestagExplorer', value: 400 },
+  { text: '#bestesprojekt', value: 400 },
+  { text: 'foo bar', value: 400 },
+];
 
 function App() {
+
+
+
+  //render(<WordCloud data={data} />, document.getElementById('root'));
+
+
   return (
     <div className="App">
+      <div><WordCloud data={data}/>
+</div>
+        
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hello Worl
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +39,9 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+
     </div>
   );
 }
