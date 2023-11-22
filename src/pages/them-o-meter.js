7c3e3447b699cf6c9c67512147e885a9e891fb17
maximilianmatscher <ge64sel>
2023-11-22 17:17:39 +0100

    Add MUI dependencies + basic router setup

---
+++ src/pages/them-o-meter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const ThemOMeter = () => {
+    return (
+        <div>
+            <h1>Welcome to the Them-o-meter</h1>
+        </div>
+    );
+};
+ 
+export default ThemOMeter;
---
+++ src/pages/voting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Voting = () => {
+    return (
+        <div>
+            <h1>Welcome to the Voting pager</h1>
+        </div>
+    );
+};
+ 
+export default Voting;
