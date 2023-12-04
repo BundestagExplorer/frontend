3637e40886c84a29c54939085f92c9aaecc3d78f
maximilianmatscher <ge64sel>
2023-12-04 22:26:25 +0100

    Add basic poll page

--- src/pages/voting.js
+++ src/pages/voting.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PollCard from '../poll/poll_card';
  
 const Voting = () => {
     return (
-        <div>
-            <h1>Welcome to the Voting pager</h1>
+        <div style={{ maxWidth: '80%', margin: '0 auto', padding: '2vh' }}>
+            <h1>Welcome to the Voting page</h1>
+
+            <PollCard
+                date="2023-12-04"
+                title="Electoral Voting Title"
+                result="accepted"
+                party="Example Party"
+                additionalInfo="Additional information about the voting..."
+                width="300px"
+                align="left"
+            />
+            <PollCard
+                date="2023-12-04"
+                title="Another Voting Title"
+                result="rejected"
+                party="Another Party"
+                additionalInfo="More details about the voting..."
+                width="400px"
+                align="right"
+            />
         </div>
     );
 };
