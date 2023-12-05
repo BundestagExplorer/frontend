a541aaa78e702c7d9e358454abfc8819b4b27576
maximilianmatscher <ge64sel>
2023-12-05 12:13:25 +0100

    Add category selection for voting

--- src/pages/voting.js
+++ src/pages/voting.js
@@ -1,27 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PollCard from '../poll/poll_card';
- 
+import { Accordion, AccordionDetails, AccordionSummary, Checkbox, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const Voting = () => {
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleCategoryToggle = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((c) => c !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
+    const handleAccordionChange = () => {
+        setExpanded(!expanded);
+    };
+
+    const votingData = [
+        {
+            id: 1,
+            date: '2023-12-04',
+            title: 'Electoral Voting Title',
+            result: 'accepted',
+            party: 'Example Party',
+            additionalInfo: 'Additional information about the voting...',
+            category: 'Finance',
+        },
+        {
+            id: 2,
+            date: '2023-12-04',
+            title: 'Another Voting Title',
+            result: 'rejected',
+            party: 'Another Party',
+            additionalInfo: 'More details about the voting...',
+            category: 'Inner politics',
+        },
+    ];
+
+    const filteredVotingData = selectedCategories.length
+        ? votingData.filter((item) => selectedCategories.includes(item.category))
+        : votingData;
+
     return (
         <div style={{ maxWidth: '80%', margin: '0 auto', padding: '2vh' }}>
             <h1>Welcome to the Voting page</h1>
 
-            <PollCard
-                date="2023-12-04"
-                title="Electoral Voting Title"
-                result="accepted"
-                party="Example Party"
-                additionalInfo="Additional information about the voting..."
-            />
-            <PollCard
-                date="2023-12-04"
-                title="Another Voting Title"
-                result="rejected"
-                party="Another Party"
-                additionalInfo="More details about the voting..."
-            />
+            {/* Category filter accordion */}
+            <Accordion style={{ marginBottom: '2vh'}} expanded={expanded} onChange={handleAccordionChange}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Filter by Category</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        {votingData.reduce((categories, item) => {
+                            if (!categories.includes(item.category)) {
+                                categories.push(item.category);
+                            }
+                            return categories;
+                        }, []).map((category) => (
+                            <label key={category} style={{ margin: '0.5vh 0' }}>
+                                <Checkbox
+                                    checked={selectedCategories.includes(category)}
+                                    onChange={() => handleCategoryToggle(category)}
+                                />
+                                {category}
+                            </label>
+                        ))}
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* Display filtered voting cards */}
+            {filteredVotingData.map((item) => (
+                <PollCard
+                    key={item.id}
+                    date={item.date}
+                    title={item.title}
+                    result={item.result}
+                    party={item.party}
+                    additionalInfo={item.additionalInfo}
+                    category={item.category}
+                />
+            ))}
         </div>
     );
 };
- 
+
 export default Voting;
--- src/poll/poll_card.js
+++ src/poll/poll_card.js
@@ -7,56 +7,59 @@ import IconButton from '@mui/material/IconButton';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 
-const PollCard = ({ date, title, result, party, additionalInfo }) => {
-    const [expanded, setExpanded] = useState(false);
+const PollCard = ({ date, title, result, party, additionalInfo, category }) => {
+  const [expanded, setExpanded] = useState(false);
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-    return (
-        <Card style={{ width: '100%', margin: '0 auto', marginBottom: '1vh' }}>
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" align="right">
-                    {date}
-                </Typography>
-                <Typography variant="h5" component="div" gutterBottom>
-                    {title}
-                </Typography>
-                <Typography variant="body1" color={result === 'accepted' ? 'primary' : 'error'} gutterBottom>
-                    Result: {result}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" gutterBottom>
-                    Proposed by: {party}
-                </Typography>
-            </CardContent>
-            {expanded && (
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary">
-                        {additionalInfo}
-                    </Typography>
-                </CardContent>
-            )}
-            <CardActions disableSpacing>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    style={{ marginRight: 'auto', marginLeft: '.5vw', marginBottom: '1vh' }}
-                    onClick={() => alert('Read More Clicked')}
-                >
-                    Read More
-                </Button>
-                <IconButton
-                    style={{ marginLeft: 'auto', transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </IconButton>
-            </CardActions>
-        </Card>
-    );
+  return (
+    <Card style={{ width: '100%', margin: '0 auto', marginBottom: '1vh' }}>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" align="right">
+          {date}
+        </Typography>
+        <Typography variant="h5" component="div" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" color={result === 'accepted' ? 'primary' : 'error'} gutterBottom>
+          Result: {result}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          Proposed by: {party}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          Resort: {category}
+        </Typography>
+      </CardContent>
+      {expanded && (
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {additionalInfo}
+          </Typography>
+        </CardContent>
+      )}
+      <CardActions disableSpacing>
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{ marginRight: 'auto', marginLeft: '.5vw', marginBottom: '1vh' }}
+          onClick={() => alert('Read More Clicked')}
+        >
+          Read More
+        </Button>
+        <IconButton
+          style={{ marginLeft: 'auto', transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default PollCard;
