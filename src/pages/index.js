641a8aa1fb02766931394709d91327faa873b341
ge49kuz <simon.strzoda@tum.de>
2023-12-18 18:07:15 +0100

    initial commit of new layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import CustomBubbleChart from '../bubblechart/bubble_chart';
+import TemporaryDrawer from '../drawer/drawer';
 
 const Home = () => {
   return (
     <div>
+      <TemporaryDrawer  />
       <CustomBubbleChart />
+      
+      
 
     </div>
   );
